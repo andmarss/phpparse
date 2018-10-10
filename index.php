1542a3e5f3bfadc62a8a47021ce6c60c8d39ae48
Марков Андрей Игоревич <anrey-markov@mail.ru>
2018-10-11 02:41:12 +0300

    + функция для перехода по статьям и парсинга данных статьи

--- index.php
+++ index.php
@@ -4,6 +4,19 @@ require_once(dirname(__FILE__) . '/vendor/autoload.php');
 
 use Sunra\PhpSimple\HtmlDomParser;
 
+function get_article_data($url) {
+    $parser = new HtmlDomParser;
+
+    $hrml = file_get_contents($url);
+
+    $article = $parser->str_get_html($hrml);
+
+    $h1 = $article->find('h1', 0)->innertext;
+    $content = $article->find('article', 0)->innertext;
+
+    return compact('h1', 'content');
+}
+
 function get_article ($url) {
     $parser = new HtmlDomParser;
 
@@ -12,7 +25,7 @@ function get_article ($url) {
     $dom = $parser->str_get_html($hrml);
 
     foreach ($dom->find('a.read-more-link') as $link) {
-        echo $link->href . PHP_EOL;
+        print_r(get_article_data($link->href));
     }
 
     if($next_link = $dom->find('a.next', 0)) {
