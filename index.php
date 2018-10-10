91d6770668cea2334cab69707b76cecc373bb7bc
Марков Андрей Игоревич <anrey-markov@mail.ru>
2018-10-11 02:23:57 +0300

    создание рекурсивной парсинг-функции

--- index.php
+++ index.php
@@ -4,10 +4,20 @@ require_once(dirname(__FILE__) . '/vendor/autoload.php');
 
 use Sunra\PhpSimple\HtmlDomParser;
 
-$parser = new HtmlDomParser;
+function get_article ($url) {
+    $parser = new HtmlDomParser;
 
-$hrml = file_get_contents('http://ananaska.com/vse-novosti/');
+    $hrml = file_get_contents($url);
 
-$dom = $parser->str_get_html($hrml);
+    $dom = $parser->str_get_html($hrml);
 
-var_dump($dom->innertext);
+    foreach ($dom->find('a.read-more-link') as $link) {
+        echo $link->href . PHP_EOL;
+    }
+
+    if($next_link = $dom->find('a.next', 0)) {
+        get_article($next_link->href);
+    }
+}
+
+get_article('http://ananaska.com/vse-novosti/');
